27ae7a31720952b37c74c63ce6d9b0dbd3114144
planimir77 <34979738+planimir77@users.noreply.github.com>
2019-11-20 23:26:30 +0200

    Car Dealer DB and ImportSuppliers Таsк

---
+++ 09.JsonProcessing/CarDealer/CarDealer/StartUp.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using AutoMapper;
+using CarDealer.Data;
+using CarDealer.Models;
+using Newtonsoft.Json;
+
+namespace CarDealer
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            using (var context = new CarDealerContext())
+            {
+                var inputJson = File.ReadAllText("./../../../Datasets/suppliers.json");
+
+                var result = ImportSuppliers(context, inputJson);
+
+                Console.WriteLine(result);
+            }
+        }
+
+        public static string ImportSuppliers(CarDealerContext context, string inputJson)
+        {
+            var suppliersData = JsonConvert.DeserializeObject<Supplier[]>(inputJson);
+
+            context.Suppliers.AddRange(suppliersData);
+
+            context.SaveChanges();
+
+            return $"Successfully imported {suppliersData.Length}.";
+        }
+    }
+}
